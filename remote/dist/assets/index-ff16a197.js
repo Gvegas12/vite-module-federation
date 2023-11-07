a2d21a9a28d39d98f2028754da421e79267da9d3
abdukarimov.h2 <abdukarimov.h2@wildberries.work>
2023-11-07 15:40:24 +0300

    feat: first commit

---
+++ remote/dist/assets/index-ff16a197.js
@@ -0,0 +1,43 @@
+import { importShared } from './__federation_fn_import.js';
+import { j as jsxs, a as jsx, Button } from './__federation_expose_Button-8c92f2c5.js';
+import { r as reactDomExports } from './__federation_shared_react-dom-6fc2ee8d.js';
+
+var client = {};
+
+var m = reactDomExports;
+{
+  client.createRoot = m.createRoot;
+  client.hydrateRoot = m.hydrateRoot;
+}
+
+const reactLogo = "/assets/react-35ef61ed.svg";
+
+const App$1 = '';
+
+const {useState} = await importShared('react');
+function App() {
+  useState(0);
+  return /* @__PURE__ */ jsxs("div", { className: "App", children: [
+    /* @__PURE__ */ jsxs("div", { children: [
+      /* @__PURE__ */ jsx("a", { href: "https://vitejs.dev", target: "_blank", children: /* @__PURE__ */ jsx("img", { src: "/vite.svg", className: "logo", alt: "Vite logo" }) }),
+      /* @__PURE__ */ jsx("a", { href: "https://reactjs.org", target: "_blank", children: /* @__PURE__ */ jsx("img", { src: reactLogo, className: "logo react", alt: "React logo" }) })
+    ] }),
+    /* @__PURE__ */ jsx("h1", { children: "Vite + React" }),
+    /* @__PURE__ */ jsxs("div", { className: "card", children: [
+      /* @__PURE__ */ jsx(Button, {}),
+      /* @__PURE__ */ jsxs("p", { children: [
+        "Edit ",
+        /* @__PURE__ */ jsx("code", { children: "src/App.jsx" }),
+        " and save to test HMR"
+      ] })
+    ] }),
+    /* @__PURE__ */ jsx("p", { className: "read-the-docs", children: "Click on the Vite and React logos to learn more" })
+  ] });
+}
+
+const index = '';
+
+const React = await importShared('react');
+client.createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsx(React.StrictMode, { children: /* @__PURE__ */ jsx(App, {}) })
+);
